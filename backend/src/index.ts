f7a5a3f33d9e08885c33ce5a8533cb997301a5af
Ankit Sharma <ankit.sharma21598@gmail.com>
2025-11-21 17:07:02 +0530

    Updated temp folder for uploads

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
-import app from "./app.js";
 import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+// import scrapeRoutes from "./routes/scrape"
+// import userRoutes from "./routes/user"
+import userRoutes from "./routes/userRoutes.js";
+import insightRoutes from "./routes/insightRoutes.js";
+import datasetRoutes from "./routes/datasetRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import cors from "cors";
 dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/scrape", scrapeRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/insights", insightRoutes);
+app.use("/api/datasets", datasetRoutes);
+app.use("/api/upload", uploadRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI || "")
